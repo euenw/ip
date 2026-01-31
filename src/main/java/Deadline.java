31c9536cefa66d17d8fb3fe51c3570e05217778a
euenw <euenwong@gmail.com>
2026-01-31 14:13:22 +0800

    Level 8. Dates and Times

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,41 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDate by;
+    protected String byString;
+    private static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("MMM d yyyy");
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        try {
+            this.by = LocalDate.parse(by, INPUT_FORMAT);
+            this.byString = null;
+        } catch (DateTimeParseException e) {
+            this.by = null;
+            this.byString = by;
+        }
     }
 
     public String getBy() {
-        return by;
+        if (by != null) {
+            return by.format(INPUT_FORMAT);
+        }
+        return byString;
+    }
+
+    public String getByForDisplay() {
+        if (by != null) {
+            return by.format(OUTPUT_FORMAT);
+        }
+        return byString;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + getByForDisplay() + ")";
     }
 }
