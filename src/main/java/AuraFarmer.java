05f31d1e81301524c316215cc894a18eb3c62574
euenw <euenwong@gmail.com>
2026-01-31 13:45:42 +0800

    Level 1. Echo

--- src/main/java/AuraFarmer.java
+++ src/main/java/AuraFarmer.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class AuraFarmer {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println(" hello! i'm aurafarmer");
-        System.out.println(" what can i do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" bye. hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    aura: hello! i'm aurafarmer");
+        System.out.println("    aura: what can i do for you?");
+        System.out.println("    ____________________________________________________________");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    aura: bye. hope to see you again soon!");
+                System.out.println("    ____________________________________________________________");
+                break;
+            }
+
+            System.out.println("    ____________________________________________________________");
+            System.out.println("    aura: " + input);
+            System.out.println("    ____________________________________________________________");
+        }
+
+        scanner.close();
     }
 }
