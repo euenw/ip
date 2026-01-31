b33e4ec99d8ecf17f9d3c69d91d7e85b7df16c2f
euenw <euenwong@gmail.com>
2026-01-31 13:43:07 +0800

    Rename, Greet, Exit

---
+++ src/main/java/AuraFarmer.java
@@ -0,0 +1,10 @@
+public class AuraFarmer {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" hello! i'm aurafarmer");
+        System.out.println(" what can i do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" bye. hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
