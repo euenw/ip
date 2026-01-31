fd89b93e66aef5ec34c8750273c65147a43e4bb1
euenw <euenwong@gmail.com>
2026-01-31 13:50:33 +0800

    Level 2. Add, List

--- src/main/java/AuraFarmer.java
+++ src/main/java/AuraFarmer.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class AuraFarmer {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         System.out.println("    ____________________________________________________________");
         System.out.println("    aura: hello! i'm aurafarmer");
@@ -14,14 +16,22 @@ public class AuraFarmer {
 
             if (input.equals("bye")) {
                 System.out.println("    ____________________________________________________________");
-                System.out.println("    aura: bye. hope to see you again soon!");
+                System.out.println("    aura: next time you return, return with more aura");
                 System.out.println("    ____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("    ____________________________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("    aura: " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("    ____________________________________________________________");
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    aura: added: " + input);
+                System.out.println("    ____________________________________________________________");
             }
-
-            System.out.println("    ____________________________________________________________");
-            System.out.println("    aura: " + input);
-            System.out.println("    ____________________________________________________________");
         }
 
         scanner.close();
